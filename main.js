5eb0e1f97d506a1b117f8a32f743c25aa4df6932
Mark Hajnal <mhajnal@sympatico.ca>
2022-04-23 21:20:55 -0400

    Event listeners added, but not called

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //let computerSelection;
 //let playerSelection;
 
-
 // computer selects rock, paper, scissors)
         function computerPlay() {  
           //let computerSelection;
@@ -11,6 +10,7 @@
                 console.log('inside randomInteger()');
                 return Math.floor(Math.random() * (max - min + 1)) + min; 
             }
+            console.log('inside computerPlay()');
 
             if (randomInteger() === 1) { //having () seems to make no difference
               //computerSelection = "rock";
@@ -29,17 +29,112 @@
             //return computerSelection;
             
         } 
+/*
+        function playerChoice() { 
+          let choice;
+          const buttons = document.querySelectorAll('button');
+          buttons.forEach((button) => button.addEventListener(('click'), (e) => {
+            choice = e.target.id;
+            console.log('choice', choice);
 
-        function playerChoice() {
-          let playerSelection = prompt("Rock, paper, or scissors?"); 
+          }));
+          console.log('choice', choice);
+
+          return choice;
+
+         /* let playerSelection = prompt("Rock, paper, or scissors?"); 
           playerSelection = playerSelection.toLowerCase();   
            while (playerSelection !=  "rock" && playerSelection !="paper" && playerSelection != "scissors") {
               console.log("Invalid choice!  Please choose rock, paper, or scissors.");
               playerSelection = prompt("Rock, paper, or scissors?"); 
               playerSelection = playerSelection.toLowerCase();
             }
-            return playerSelection;
-        }  
+            return playerSelection;*/
+      //  }  */
+
+        function getChoices() {
+          let choices = [];
+          const rock = document.querySelector('#rock');
+          const paper = document.querySelector('#paper');
+          const scissors = document.querySelector("#scissors");
+
+          console.log('inside getChoices()');
+
+          rock.addEventListener('click', () => {
+            console.log('rock');
+            return 'rock'});
+          paper.addEventListener('click', () => {return 'paper'});
+          scissors.addEventListener('click', () => {return 'scissors'});
+
+          /*const buttons = document.querySelectorAll('button');
+          buttons.forEach(button => button.addEventListener(('click'), (e) => {
+            if (e.target.id) {
+              choices.push(e.target.id);
+              choices.push(computerPlay());
+            }
+          }));*/
+          //return choices;
+        }
+
+        function playGame(getChoices, computerPlay) {
+          //let choices = getChoices();
+          /*console.log(choices); //empty array
+          let [playerSelection, computerSelection] = choices;*/
+          let playerSelection = getChoices;
+          let computerSelection = computerPlay;
+          /*let playerSelection = getChoices;
+          let computerSelection = computerPlay;*/
+          console.log('inside playGame()');
+
+          function playerWins(playerSelection, computerSelection) {
+            result = `Computer loses!  ${playerSelection} beats ${computerSelection}`;
+            ++playerPoints;  
+            return result;
+        } 
+        
+        function computerWins(playerSelection, computerSelection) {
+              result = `You lose! ${computerSelection} beats ${playerSelection}`
+              ++computerPoints;  
+              return result;
+        } 
+
+        function tie(playerSelection, computerSelection) {
+            result = `It's a tie!`;
+            return result;
+          }  
+
+        if (playerSelection === computerSelection) { 
+          result = tie(playerSelection, computerSelection);
+          return result;
+        
+        }  else if (playerSelection ==="rock" && computerSelection === "paper") {
+          result = computerWins(playerSelection, computerSelection);
+          return result;
+
+        }  else if (playerSelection === "rock"  && computerSelection === "scissors") {
+          result = playerWins(playerSelection, computerSelection);
+          return result;
+
+
+        } else if (playerSelection === "paper"  && computerSelection === "scissors") {
+          result = computerWins(playerSelection, computerSelection);
+          return result;
+
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+          result = playerWins(playerSelection, computerSelection);
+          return result;
+
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+          result = playerWins(playerSelection, computerSelection); 
+          return result;
+
+        }  else if (playerSelection === "scissors" && computerSelection === "rock") {
+          result = computerWins(playerSelection, computerSelection);
+          return result;
+        } 
+
+    }
+      
 
         
         //let playerWins;
@@ -54,8 +149,8 @@
 
             console.log('inside playOneRound()');
             let result;
-
-            let computerSelection = computerPlay();
+            
+            let computerSelection = computerPlay();//called before button clicked;
             let playerSelection = playerChoice();
 
             function playerWins(playerSelection, computerSelection) {
@@ -134,4 +229,9 @@ function game() {  //only declares winner every other time??
 } 
           
 game();*/
-console.log(playOneRound());
+//console.log(playOneRound());
+
+let choices = getChoices;
+let chomputerChoices = computerPlay();
+console.log(playGame(choices, computerPlay));
+//console.log(playGame(getChoices, computerPlay));
